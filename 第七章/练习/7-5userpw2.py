16026b80c401801c3f192b3f8f23c2a774678d66
jaredchin <1007707989@qq.com>
2017-11-23 21:07:39 +0800

    test

--- 第七章/练习/7-5userpw2.py
+++ 第七章/练习/7-5userpw2.py
@@ -1,4 +1,7 @@
+import hashlib
+
 db = {}
+
 
 def newuser():
     prompt = 'login desired:'
@@ -10,23 +13,48 @@ def newuser():
         else:
             break
     pwd = input('passwd:')
-    db[name] = pwd
+    db[name] = encodepass(pwd)
 
 
 def olduser():
     name = input('login:')
     pwd = input('password:')
     passwd = db.get(name)
-    if passwd == pwd:
+    if passwd == encodepass(pwd):
         print('welcome back, %s' % name)
     else:
         print('login incorrect')
+
+
+def deluser():
+    name = input('Del user: ')
+    if name in db:
+        del db[name]
+        print('user %s has delete.' % name)
+    else:
+        print('user %s is not valid!' % name)
+
+
+def viewuser():
+    if db == {}:
+        print('no user regist!')
+    else:
+        for key in db:
+            print('user:%s passwd:%s' % (key, db[key]))
+
+
+def encodepass(passwd):
+    m = hashlib.md5()
+    m.update(passwd.encode(encoding="utf-8"))
+    return m.hexdigest()
 
 
 def showmenu():
     prompt = """
     (N)ew User Login
     (E)xisting User Login
+    (D)elete user
+    (V)eiw all user
     (Q)uit
     Enter choice:"""
 
@@ -39,7 +67,7 @@ def showmenu():
             except(EOFError, KeyboardInterrupt):
                 choice = 'q'
             print('\nYou picked: [%s]' % choice)
-            if choice not in 'neq':
+            if choice not in 'neqdv':
                 print('Invalid option, try again')
             else:
                 chosen = True
@@ -47,6 +75,8 @@ def showmenu():
         if choice == 'q': done = True
         if choice == 'n': newuser()
         if choice == 'e': olduser()
+        if choice == 'd': deluser()
+        if choice == 'v': viewuser()
 
 if __name__ == '__main__':
     showmenu()
