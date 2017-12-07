c43b5835db1e6d49002f697f6e1076cd111fd6a7
jaredchin <1007707989@qq.com>
2017-12-07 17:49:31 +0800

    test

---
+++ 第十章/10.1cardrun.py
@@ -0,0 +1,9 @@
+
+
+def safe_float(obj):
+    'safe version of float()'
+    try:
+        retval = float(obj)
+    except (ValueError, TypeError), diag:
+        retval = str(diag)
+    return retval
